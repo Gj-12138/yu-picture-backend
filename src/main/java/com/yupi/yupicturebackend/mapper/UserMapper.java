c195cf5fd19d49a6dbfad23e53c99ff80f6e9335
Gj <2964820196@qq.com>
2024-12-16 17:28:22 +0800

    用户-登录、注册、注销、管理以及用户权限控制和用户登录状态获取

---
+++ src/main/java/com/yupi/yupicturebackend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.yupicturebackend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.yupicturebackend.model.entity.User;
+
+/**
+* @author 14908
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2024-12-14 16:23:24
+* @Entity com.yupi.yupicturebackend.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ src/test/java/com/yupi/yupicturebackend/service/impl/UserServiceImplTest.java
@@ -0,0 +1,23 @@
+package com.yupi.yupicturebackend.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+
+class UserServiceImplTest {
+
+    @Test
+    void userRegister() {
+    }
+
+    @Test
+    void getEncryptPassword() {
+    }
+
+    @Test
+    void userLogin() {
+    }
+
+    @Test
+    void getLoginUser() {
+    }
+}
